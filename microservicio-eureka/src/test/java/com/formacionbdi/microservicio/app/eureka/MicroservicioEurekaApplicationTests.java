8fddd5bdc7e53da3a5b5394e6edcc6854fa34d68
mauriciomunoz-coder <arnoldmunoz@ustadistancia.edu.co>
2022-03-03 10:33:39 -0500

    Add files via upload

---
+++ microservicio-eureka/src/test/java/com/formacionbdi/microservicio/app/eureka/MicroservicioEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.microservicio.app.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
